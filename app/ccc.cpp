da06a94edf631d0944d04eef4e00f51e96fe76e8
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-06-18 11:03:01 +0200

    Added TuiImpl class.

--- app/ccc.cpp
+++ app/ccc.cpp
@@ -4,8 +4,13 @@
 
 int main()
 {
+    /*
     std::cout << "Using libMsgService " << CCC::MsgService::version() << std::endl;
     std::cout << "Using libTui " << CCC::Tui::version() << std::endl;
+    */
+
+  CCC::Tui::Tui ui;
+    ui.run();
 
     return EXIT_SUCCESS;
 }
